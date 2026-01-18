070fbef826d9b19e5e32a7b1a298f6ce44097e22
RAVS <shubham110267@gmail.com>
2026-01-19 01:44:20 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,18 +1,43 @@
-const scriptURL="https://script.google.com/macros/s/AKfycbxP_S2WCzLUz3PXKFX5pXFsARmAONM3simdeaDtOyPFfb0z06-8dd3ShHOry7d5IKL0PA/exec";
-const forms=document.querySelectorAll("#contactForm");
-forms.forEach(form=>{
-  const msg=form.querySelector("#formMsg");
-  form.addEventListener("submit",e=>{
-    e.preventDefault();
-    const formData={
-      name:form.name.value,
-      email:form.email.value,
-      phone:form.phone.value,
-      service:form.service.value,
-      message:form.message.value
-    };
-    fetch(scriptURL,{method:"POST",body:JSON.stringify(formData),mode:"no-cors"})
-    .then(()=>{msg.style.color="green";msg.innerText="Thank you! Your message has been submitted.";form.reset();setTimeout(()=>{msg.innerText="";},5000);})
-    .catch(()=>{msg.style.color="red";msg.innerText="Error submitting form. Please try again.";});
+<script>
+const scriptURL = "https://script.google.com/macros/s/AKfycbxP_S2WCzLUz3PXKFX5pXFsARmAONM3simdeaDtOyPFfb0z06-8dd3ShHOry7d5IKL0PA/exec";
+
+const form = document.getElementById("contactForm");
+const msg = document.getElementById("formMsg");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  msg.style.color = "black";
+  msg.innerText = "Submitting...";
+
+  const formData = {
+    name: form.name.value,
+    email: form.email.value,
+    phone: form.phone.value,
+    message: form.message.value
+  };
+
+  fetch(scriptURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.status === "success") {
+      msg.style.color = "green";
+      msg.innerText = "Thank you for contacting us. We will connect you shortly.";
+      form.reset();
+      setTimeout(() => msg.innerText = "", 5000);
+    } else {
+      throw new Error("Submission failed");
+    }
+  })
+  .catch(err => {
+    msg.style.color = "red";
+    msg.innerText = "Error submitting form. Please try again.";
   });
 });
+</script>
